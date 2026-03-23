2b08cbe645be55f22c3d752b75b451f9a844eed0
BPieterson <ST10519794@myemeris.edu.za>
2026-03-23 13:21:32 +0200

    main

--- app/src/main/java/com/example/socailspark/MainActivity.kt
+++ app/src/main/java/com/example/socailspark/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     // Variables
     lateinit var txtTOD: EditText
-    lateinit var txtResult: TextView
+    lateinit var tvResult: TextView
     lateinit var btnStart: Button
     lateinit var btnClear: Button
 
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         txtTOD = findViewById(R.id.txtTOD)
-        txtResult = findViewById(R.id.txtResult)
+        tvResult = findViewById(R.id.tvResult)
         btnStart = findViewById(R.id.btnStart)
         btnClear = findViewById(R.id.btnClear)
 
@@ -38,31 +38,31 @@ class MainActivity : AppCompatActivity() {
             // This is from text color change is from Gabriele Mariotti's answer on StackOverflow:
             // https://stackoverflow.com/questions/5679138/how-to-change-the-text-color-of-a-textview-in-android
             val tod = txtTOD.text.toString()
-            txtResult.setTextColor(getColor(R.color.black))
+            tvResult.setTextColor(getColor(R.color.black))
             if(tod == m) {
-                txtResult.text = "$m: Send a 'Good Morning' message to all your friends and family."
+                tvResult.text = "$m: Send a 'Good Morning' message to all your friends and family."
             } else if (tod == mm) {
-                txtResult.text = "$mm: Reach out to a colleague with a quick thank you."
+                tvResult.text = "$mm: Reach out to a colleague with a quick thank you."
             } else if (tod == af) {
-                txtResult.text = "$af: Share a funny meme or interesting link with a friend or family member"
+                tvResult.text = "$af: Share a funny meme or interesting link with a friend or family member"
             } else if (tod == asn) {
-                txtResult.text = "$asn: Send a Quick 'thinking of you' message"
+                tvResult.text = "$asn: Send a Quick 'thinking of you' message"
             } else if (tod == d) {
-                txtResult.text = "$d: Call a friend or a relative for 5-min cath-up"
+                tvResult.text = "$d: Call a friend or a relative for 5-min cath-up"
             } else if (tod == ad) {
-                txtResult.text = "$ad/$n: Leave a thoughtful comment on a friend's post or message"
+                tvResult.text = "$ad/$n: Leave a thoughtful comment on a friend's post or message"
             } else if (tod == n) {
-                txtResult.text = "$ad/$n: Leave a thoughtful comment on a friend's post or message"
+                tvResult.text = "$ad/$n: Leave a thoughtful comment on a friend's post or message"
             } else {
 
-                txtResult.setTextColor(getColor(R.color.red))
-                txtResult.text = "$tod!!! Please enter the valid time of day from the keywords above!!!"
+                tvResult.setTextColor(getColor(R.color.red))
+                tvResult.text = "$tod!!! Please enter the valid time of day from the keywords above!!!"
             }
         }
         //Clear the text fields
         btnClear.setOnClickListener {
             txtTOD.text.clear()
-            txtResult.text = " "
+            tvResult.text = " "
         }
     }
 }
